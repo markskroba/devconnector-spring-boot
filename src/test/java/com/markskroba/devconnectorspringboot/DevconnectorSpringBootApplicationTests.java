d0e3ba1b24b7d3827198ab645c33fb6da98fb8a3
Mark Skroba <mark.skroba@slu.edu>
2023-04-22 14:04:23 -0500

    initial commit

---
+++ src/test/java/com/markskroba/devconnectorspringboot/DevconnectorSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.markskroba.devconnectorspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevconnectorSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
